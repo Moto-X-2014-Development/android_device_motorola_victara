aee847a0ebc2652dfd78d51e378d36ddf044771d
jro1979oliver <jroliveira.oliveira301@gmail.com>
2020-02-05 13:16:22 +0000

    Havoc time

--- device.mk
+++ device.mk
@@ -351,3 +351,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_cfg.dat:$(TARGET_COPY_OUT_SYSTEM)/etc/firmware/wlan/prima/WCNSS_cfg.dat \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/firmware/wlan/prima/WCNSS_qcom_cfg.ini \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_wlan_nv.bin:$(TARGET_COPY_OUT_SYSTEM)/etc/firmware/wlan/prima/WCNSS_qcom_wlan_nv.bin
+
+# AOSP packages
+PRODUCT_PACKAGES += \
+    Recorder \
+    Terminal \
+    messaging \
+    LatinIME
